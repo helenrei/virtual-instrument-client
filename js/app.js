be8fd05d032e58242f2811836630bd84cd1eb80b
ooker <maaker+github@gmail.com>
2021-12-12 17:30:29 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//const socket = io();
+
+const socket = io("http://localhost:8000");
+
+
+// server
+socket.on("connect", () => {
+  console.log("Client connected to: " + socket.id); // x8WIv7-mJelg7on_ALbx
+  online = true;
+});
+
+socket.on('send-data', res => {
+  recieveData(res); 
+});
+
+
+socket.on("disconnect", () => {
+  console.log("Client disconnected from" + socket.id); 
+});
